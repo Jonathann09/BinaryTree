ca1e475b4f0a50de8b995d8f71540a003545934d
Jonathann09 <jonathanurrete005@gmail.com>
2023-01-15 12:47:10 +0800

    BinarySearchTreeNode

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,2 +1,8 @@
 print("===JONATHAN U. URRETE===")
 print("===BSCOE 2-2===")
+
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
