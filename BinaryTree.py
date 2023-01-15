f55e342dbe8c3e54410d746fdd0b17000f8705e8
Jonathann09 <jonathanurrete005@gmail.com>
2023-01-15 13:15:32 +0800

    visit base node, visit right tree

--- BinaryTree.py
+++ BinaryTree.py
@@ -43,6 +43,13 @@ class BinarySearchTreeNode:
         if self.left:
             elements += self.left.in_order_traversal()  
         
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
+        
+        return elements
+    
                        
          
     
