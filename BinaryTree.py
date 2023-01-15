80c81ead2c18a86e4015f2aae36d828fd658cf47
Jonathann09 <jonathanurrete005@gmail.com>
2023-01-15 13:19:25 +0800

    pre order traversal

--- BinaryTree.py
+++ BinaryTree.py
@@ -60,7 +60,16 @@ class BinarySearchTreeNode:
         elements.append(self.data)
 
         return elements
-                       
+    
+    def pre_order_traversal(self):
+        elements = [self.data]
+        if self.left:
+            elements += self.left.pre_order_traversal()
+        if self.right:
+            elements += self.right.pre_order_traversal()
+
+        return elements
+                     
          
     
         
