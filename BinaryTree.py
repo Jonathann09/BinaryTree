83a9f5328912a0a22cb8e6f4586c0494decb20db
Jonathann09 <jonathanurrete005@gmail.com>
2023-01-15 12:54:31 +0800

    adding data

--- BinaryTree.py
+++ BinaryTree.py
@@ -6,3 +6,11 @@ class BinarySearchTreeNode:
         self.data = data
         self.left = None
         self.right = None
+    
+    def add_child(self, data):
+        if data == self.data:
+            return  # node already exist   
+         
+    
+        
+        
